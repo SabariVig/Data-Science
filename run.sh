1a0769e891e4883a273f14d6f4041fda1d1536ae
SabariVig <blazingsabari@gmail.com>
2018-07-22 16:37:08 +0530

    matrix plot

--- run.sh
+++ run.sh
@@ -3,14 +3,17 @@ echo "Enter The Commit Message"
 read  s
 git add .
 git commit -a -m"$s"
+echo "Git Commited With Message  " $s
 echo "Do You Want To Push TO GitHub.Com[y/n]"
 read c
-if[ $c == 'y'];then
-git push origin master
+if [ $c == 'y' ]
+then
+  git push origin master
+  echo "Git Pushed Successfully-U"
 else
-echo "Commit The Changes Later"
+  echo "Commit The Changes ASAP"
 fi
-exit
+
 
 
 
