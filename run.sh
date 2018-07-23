480668dcec305699a4615d4ac5b159071ab27b7a
SabariVig <blazingsabari@gmail.com>
2018-07-23 22:47:02 +0530

    Pandas Visualization
    Using Inbuilt Pandas Data Visualization

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ echo "Enter Commit Description"
 read d
 git add .
 git commit -a -m"$s" -m"$d"
+tput setaf 1
 echo "Git Commited With Message  " $s
+tput sgr0
 git status
 echo "Do You Want To Push TO GitHub.Com[y/n]"
 read c
@@ -13,7 +15,7 @@ if [ $c == 'y' ]
 then
   git push origin master
   tput setaf 4
-  echo "Git Pushed Successfully-U"
+  echo "Git Pushed Successfully"
   tput sgr0
 else
    tput setaf 1
